32cdd7e6227e81ace3b3e9911c581fbe048a8855
sahildeep931065 <sahildeep9301@gmail.com>
2025-06-26 05:53:55 +0530

    Fix login route

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ import auth from "./route/auth.js";
 dotenv.config();
 
 const app = express();
+
+
+app.use(cors({
+  origin: 'https://casa-direct.netlify.app/',  
+  credentials: true,
+}));
 app.use(cors())
 app.use(express.json());
 app.use('/api', auth)
